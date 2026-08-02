2317523deeacc7faa03aae84ec92e377628984c5
Vision Thapa <133334975+VLEAIT@users.noreply.github.com>
2026-08-02 04:57:29 +0000

    chore: update application model

--- models/applications.py
+++ models/applications.py
@@ -24,5 +24,15 @@ class Application(Base,TimeStampMixin,kw_only=True):
     district:Mapped[str] = mapped_column(String(50),nullable=False,index=True,comment="District where land parcel is located")
     land_area_hectares:Mapped[float] = mapped_column(nullable=False,comment="Land area in hectares")
     land_type:Mapped[str] = mapped_column(String(20),nullable=False,comment="Type of land parcel (Khet(irrigated), Bari(rain-fed), GharBari(homestead))")
+    land_grade:Mapped[str] = mapped_column(String(20),nullable=False,comment="Fertility Grade of land parcel (Aabal(1st), Doyam(2nd), Sim(3rd), Chahar(4th))")
+    coop_income_monthly:Mapped[float] = mapped_column(Float,nullable=False,comment="Monthly income of the farmer from cooperative farming")
+    remitance_monthly:Mapped[float] = mapped_column(Float,nullable=False,default=0.0,comment="Monthly remittance received by the farmer")
+    remittance_channel:Mapped[str] = mapped_column(String(20),nullable=False,default='None',comment="Channel through which remittance is received (IME, Prabhu, Hundi, None)")
+    requested_amount:Mapped[float] = mapped_column(Float,nullable=False,comment="Amount requested by the user in nrs")
+    consent_given:Mapped[bool] = mapped_column(Boolean,nullable=False,default=False,comment="Whether the user has given consent for data processing")
+    application_status:Mapped[str] = mapped_column(String(20),index=True,nullable=False,default='Pending',comment="Status of the application (Pending, Processing, Completed, Failed)")
+
+    decision:Mapped["Decision"] = relationship("Decision",back_populates="application",uselist=False,init=False,cascade="all, delete-orphan")
+    audit_logs:Mapped[list["AuditLog"]] = relationship("AuditLog",back_populates="application",init=False,order_by="AuditLog.created_at",cascade="all, delete-orphan")
 
 
