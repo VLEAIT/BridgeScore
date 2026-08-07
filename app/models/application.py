ae09d4bb58c9c3e7c6039596b8d89262a5793e76
Vision Thapa <133334975+VLEAIT@users.noreply.github.com>
2026-08-07 13:30:45 +0000

    fix: creation of lalapurja data dict and citizenship number

--- app/models/application.py
+++ app/models/application.py
@@ -2,6 +2,8 @@ from sqlalchemy import CheckConstraint, String, Float, Boolean
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 from database import Base, TimeStampMixin
 import uuid
+from sqlalchemy.dialects.postgresql import JSONB
+from typing import Dict,Any,Optional
 
 
 class Application(Base, TimeStampMixin, kw_only=True):
@@ -21,13 +23,15 @@ class Application(Base, TimeStampMixin, kw_only=True):
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default_factory=uuid.uuid4, init=False)
     farmer_name: Mapped[str] = mapped_column(String(100), nullable=False, index=True, comment="Name of the farmer same as citizenship name")
     district: Mapped[str] = mapped_column(String(50), nullable=False, index=True, comment="District where land parcel is located")
+    citizenship_number:Mapped[str]=mapped_column(String(50),nullable=False,index=True,comment="Offical citizen number of the applicant")
     land_area_hectares: Mapped[float] = mapped_column(nullable=False, comment="Land area in hectares")
     land_type: Mapped[str] = mapped_column(String(20), nullable=False, comment="Type of land parcel (Khet(irrigated), Bari(rain-fed), GharBari(homestead))")
     land_grade: Mapped[str] = mapped_column(String(20), nullable=False, comment="Fertility Grade of land parcel (Aabal(1st), Doyam(2nd), Sim(3rd), Chahar(4th))")
     coop_income_monthly: Mapped[float] = mapped_column(Float, nullable=False, comment="Monthly income of the farmer from cooperative farming")
     remittance_monthly: Mapped[float] = mapped_column("remittance_monthly", Float, nullable=False, default=0.0, comment="Monthly remittance received by the farmer")
     remittance_channel: Mapped[str] = mapped_column(String(20), nullable=False, default="None", comment="Channel through which remittance is received (IME, Prabhu, Hundi, None)")
-    requested_amount: Mapped[float] = mapped_column(Float, nullable=False, comment="Amount requested by the user in nrs")
+    requested_amount: Mapped[float] = mapped_column(float, nullable=False, comment="Amount requested by the user in nrs")
+    lalpurja_data:Mapped[Optional[Dict[str,Any]]]=mapped_column(JSONB,nullable=True,default=None,comment="Structured fields extracted from lalpurja by DVA-kitta,owner,area,grade")
     consent_given: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False, comment="Whether the user has given consent for data processing")
     status: Mapped[str] = mapped_column("application_status", String(20), index=True, nullable=False, default="Pending", comment="Status of the application (Pending, Processing, Completed, Rejected)")
 
