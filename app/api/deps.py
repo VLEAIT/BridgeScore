c73193d8d8d512e097f119299704cd9c9b990ab0
Vision Thapa <133334975+VLEAIT@users.noreply.github.com>
2026-08-08 12:31:24 +0000

    chore: Solved the problem of sessionlocal import

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,11 +1,11 @@
 from typing import Generator 
 from sqlalchemy.orm import Session
-from app.db.base import SessionLocal
+from app.db.database import SessionLocal
 
 def get_db()->Generator[Session,None,None]:
-    db=SessionLocal
+    db = SessionLocal()
     try:
         yield db
     finally:
-        db.close()    
+        db.close()
 
