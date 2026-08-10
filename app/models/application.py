6bd7bd562aeaa1507ebd062c37d90f194c3b321a
Vision Thapa <133334975+VLEAIT@users.noreply.github.com>
2026-08-10 13:14:34 +0000

    chore: decimal and numeric addition for monetary

--- app/models/application.py
+++ app/models/application.py
@@ -4,6 +4,8 @@ from app.db.database import Base, TimeStampMixin
 import uuid
 from sqlalchemy.dialects.postgresql import JSONB
 from typing import Dict,Any,Optional
+from sqlalchemy import Numeric,Float
+from decimal import Decimal
 
 
 class Application(Base, TimeStampMixin, kw_only=True):
@@ -25,17 +27,17 @@ class Application(Base, TimeStampMixin, kw_only=True):
     district: Mapped[str] = mapped_column(String(50), nullable=False, index=True, comment="District where land parcel is located")
     citizenship_number:Mapped[str]=mapped_column(String(50),nullable=False,index=True,comment="Offical citizen number of the applicant")
     phone_number:Mapped[str]=mapped_column(String(15),nullable=False,comment="Contact number for SMS delivery")
-    land_area_hectares: Mapped[float] = mapped_column(nullable=False, comment="Land area in hectares")
+    land_area_hectares: Mapped[float] = mapped_column(Float,nullable=False, comment="Land area in hectares")
     land_type: Mapped[str] = mapped_column(String(20), nullable=False, comment="Type of land parcel (Khet(irrigated), Bari(rain-fed), GharBari(homestead))")
     land_grade: Mapped[str] = mapped_column(String(20), nullable=False, comment="Fertility Grade of land parcel (Aabal(1st), Doyam(2nd), Sim(3rd), Chahar(4th))")
-    coop_income_monthly: Mapped[float] = mapped_column(Float, nullable=False, comment="Monthly income of the farmer from cooperative farming")
-    remittance_monthly: Mapped[Optional[float]] = mapped_column( Float, nullable=True, default=None, comment="Monthly remittance received by the farmer")
+    coop_income_monthly: Mapped[Decimal] = mapped_column(Numeric(precision=12,scale=2), nullable=False, comment="Monthly income of the farmer from cooperative farming")
+    remittance_monthly: Mapped[Optional[Decimal]] = mapped_column(Numeric(precision=12,scale=2), nullable=True, default=None, comment="Monthly remittance received by the farmer")
     remittance_channel: Mapped[str] = mapped_column(String(20), nullable=False, default="None", comment="Channel through which remittance is received (IME, Prabhu, Hundi, None)")
-    requested_amount: Mapped[float] = mapped_column(float, nullable=False, comment="Amount requested by the user in nrs")
+    requested_amount: Mapped[Decimal] = mapped_column(Numeric(precision=12,scale=2), nullable=False, comment="Amount requested by the user in nrs")
     lalpurja_data:Mapped[Optional[Dict[str,Any]]]=mapped_column(JSONB,nullable=True,default=None,comment="Structured fields extracted from lalpurja by DVA-kitta,owner,area,grade")
     lalpurja_image_path:Mapped[Optional[str]]=mapped_column(String(255),nullable=True,default=None,comment="File path of uploaded lalpurja image processed by DVA ")
     consent_given: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False, comment="Whether the user has given consent for data processing")
-    status: Mapped[str] = mapped_column( String(20), index=True, nullable=False, default="Pending", comment="Status of the application (Pending, Processing, Completed, Rejected)")
+    status: Mapped[str] = mapped_column(String(20), index=True, nullable=False, default="Pending", comment="Status of the application (Pending, Processing, Completed, Rejected)")
 
     decision: Mapped["Decision"] = relationship("Decision", back_populates="application", uselist=False, init=False, cascade="all, delete-orphan")
     audit_logs: Mapped[list["AuditLog"]] = relationship("AuditLog", back_populates="application", init=False, order_by="AuditLog.created_at", cascade="all, delete-orphan")
