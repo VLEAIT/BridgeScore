1c7c4b0eee87fe9146cf13a3b8ec7df9206bc43b
Vision Thapa <133334975+VLEAIT@users.noreply.github.com>
2026-09-14 13:00:20 +0000

    feat: complete redis_event_bus fro multiple worker archieture for production

--- app/core/redis_event_bus.py
+++ app/core/redis_event_bus.py
@@ -22,6 +22,51 @@ class RedisEventBus:
     async def close_pool(self):
         if self.pool:
             await self.pool.disconnect() 
-    
-            
+
+    def _get_client(self)->redis.Redis:
+        if self.pool is None:
+            self.pool =redis.ConnectionPool.from_url(self.redis_url,decode_responses=True)
+
+        return redis.Redis(connection_pool=self.pool)
+
+    async def publish(self,stream_id:str,event:dict)->bool:
+        try:
+            client=self._get_client()
+            payload=json.dump(event)
+            await client.publish(f"sse:{stream_id}",payload)
+            return True
+        except Exception as e:
+            logger.error(f"Failed to publish event to Redis from stream {stream_id}:{e}")  
+            return False      
+    async def listen(self,stream_id:str)->AsyncGenerator[dict,None]:
+        client=self._get_client()
+        pubsub=client.pubsub() 
+        await pubsub.subscribe(f"sse:{stream_id}")   
+
+        try:
+            while True:
+                message =await pubsub.get_message(
+                    ignore_subscribe_messages=True,
+                    timeone=1.0
+                )    
+                if message and message["typo"]=="message":
+                    data=json.loads(message["data"])
+                    if data.get("__signal__")=="close":
+                        break
+                    yield data
+
+                    await asyncio.sleep(0.01)
+        finally:
+            await  pubsub.unsubscribe(f"sse:{stream_id}")
+            await pubsub.close()        
+
+    async def close(self,stream_id:str):
+        await self.pulish(stream_id,{"__signal__":"close"})
+
+redis_event_bus=RedisEventBus()
+
+
+
+
+
        
