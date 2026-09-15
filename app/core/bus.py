bf63194e43d11a9b4cabea38a3bc7deb2ac8cba1
Vision Thapa <133334975+VLEAIT@users.noreply.github.com>
2026-09-15 03:15:42 +0000

    feat: trigger for sse formator

--- app/core/bus.py
+++ app/core/bus.py
@@ -2,4 +2,14 @@ import os
 import logging
 from app.core.event_bus import event_bus
 from app.core.redis_event_bus import redis_event_bus
+from app.core.bus_interface import BaseEventBus
+from app.core.config import settings
 
+logger=logging.getLogger(__name__)
+
+def get_event_bus()->BaseEventBus:
+    if getattr(settings,"production",False):
+        return redis_event_bus
+
+    return event_bus    
+   
